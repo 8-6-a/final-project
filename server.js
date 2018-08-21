eafef47e90b0ad80aadfe670bc0dabcead8a21dc
Lauren Holcomb <laurenholcomb@Laurens-iMac.attlocal.net>
2018-08-20 21:50:58 -0500

    added database changes

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ let passport = require('passport');
 const bodyParser = require("body-parser");
 const cors = require('cors')
 
-mongoose.connect('mongodb://marchon:march1234@ds217452.mlab.com:17452/final-project');
+mongoose.connect('mongodb://isaac:abc123@ds125872.mlab.com:25872/the-group-db'),{ useNewUrlParser: true };
 
 // express config
 const app = express();
@@ -16,12 +16,12 @@ app.use(passport.initialize());
 
 // !!! DEVELOPMENT ONLY (start) !!! //
 
-// var corsOptions = {
-//     origin: 'http://localhost:8080',
-//     optionsSuccessStatus: 200 
-// }
+var corsOptions = {
+     origin: 'http://localhost:8080',
+     optionsSuccessStatus: 200 
+ }
   
-// app.use(cors(corsOptions))
+ app.use(cors(corsOptions))
 
 // !!! DEVELOPMENT ONLY (end) !!! //
 
