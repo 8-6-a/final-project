a655e4fe036122a8f6e8137a51f134f7e07ac149
Scott Mercer <randall.mercer@yahoo.com>
2018-09-20 22:57:28 -0500

    fixed the messages to show in a header inside the footer

--- src/app/prayers/prayers.component.ts
+++ src/app/prayers/prayers.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import { PrayersService } from "../prayers.service";
 import { Router } from "@angular/router";
-import { FlashMessagesService } from "angular2-flash-messages";
+import { async } from "@angular/core/testing";
+
+
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+};
 
 @Component({
   selector: "app-prayers",
@@ -9,39 +14,38 @@ import { FlashMessagesService } from "angular2-flash-messages";
   styleUrls: ["./prayers.component.css"],
   encapsulation: ViewEncapsulation.None
 })
+
 export class PrayersComponent implements OnInit {
   prayers: any;
   prayer: any;
   userId: any;
+  header: any;
 
   // creates a new prayer
   create() {
     this.PrayersService.createPrayers({ prayer: this.prayer, userId: this.userId }).subscribe(() => {
-      window.location.reload();
-      this._flashMessagesService.show(
-        "You created a Prayer, View it in Active Prayers",
-        { cssClass: "alert-success", timeout: 10000 }
-      );
-      console.log("created a prayer");
-    });
+      this.header = " You created a Prayer, View it in Active Prayers "
+      sleep(2000).then(() => {
+        window.location.reload() 
+        })
+    })
   }
+
 
   // edits a current prayer
   edit(id, description, userId) {
     if (userId !== this.userId) {
-      alert("not allowed")
+      this.header = "Not Allowed"
     } else {
-      console.log("edit prayers");
       const newPrayer = window.prompt(`Update Prayer: ${description}`);
       if (!newPrayer) {
         return
       }
       this.PrayersService.editPrayers(id, newPrayer, userId).subscribe(() => {
-        window.location.reload();
-        this._flashMessagesService.show(
-          "You have edited a Prayer, View it in Active Prayers",
-          { cssClass: "alert-success", timeout: 10000 }
-        );
+        this.header = " You have edited your Prayer "
+        sleep(2000).then(() => {
+          window.location.reload() 
+          })
       });
     }
   }
@@ -49,15 +53,15 @@ export class PrayersComponent implements OnInit {
   // answers a prayer
   answeredPrayer(id, userId) {
     if (userId !== this.userId) {
-      alert("not allowed");
+      this.header = " Not Allowed"
     } else {
       this.PrayersService.markPrayerAnswered(id).subscribe(() => {
-        window.location.reload();
-        this._flashMessagesService.show(
-          "Praise God! Your prayer was answered, View it in Testimonies",
-          { cssClass: "alert-success", timeout: 10000 }
-        );
-      });
+        this.header =
+        "Praise God! Your prayer was answered, View it in Testimonies"
+        sleep(2000).then(() => {
+        window.location.reload() 
+        })
+      })
     }
   }
 
@@ -65,22 +69,21 @@ export class PrayersComponent implements OnInit {
   delete(id) {
     this.PrayersService.deletePrayers(id).subscribe(() => {
       window.location.reload();
+      event.preventDefault();
     });
   }
 
   // logs the user out of the session by deleting the token
   logout() {
-    localStorage.removeItem("token");
-    this.router.navigate(["/login"]);
-    this._flashMessagesService.show(
-      "You are logged out, Please log in to view Prayers",
-      { cssClass: "alert-danger", timeout: 10000 }
-    );
+    localStorage.removeItem("token")
+    this.header = " You are being logged out "
+    sleep(2000).then(() => {
+    this.router.navigate(["/login"])
+    })
   }
 
   // makes the header links scroll to the page section it corresponds to
   scrollToElement($element): void {
-    console.log($element);
     $element.scrollIntoView({
       behavior: "smooth",
       block: "start",
@@ -89,24 +92,25 @@ export class PrayersComponent implements OnInit {
   }
 
   constructor(
-    private _flashMessagesService: FlashMessagesService,
     private PrayersService: PrayersService,
-    private router: Router
+    private router: Router,
+    
   ) {
+
     this.PrayersService.getPrayers().subscribe((data: any) => {
-      console.log(data);
+      //console.log(data);
       this.prayers = data;
     });
+
     {
       let token = window.localStorage.token;
       let payload = token ? JSON.parse(window.atob(token.split(".")[1])) : null;
-      console.log(payload.id); // prints out user id
       this.userId = payload.id;
-      console.log(this.userId);
     }
   }
 
   ngOnInit() {
-    this.prayer = {};
+    this.prayer = {}
+    this.header = ' ...for My house will be called a house of Prayer for the nations.  Isaiah 56:7 '
   }
 }
